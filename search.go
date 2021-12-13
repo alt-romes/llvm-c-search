54ee28763bf91af606dbcc0f9f5066c7a2e2a6e6
romes <rodrigo.m.mesquita@gmail.com>
2021-12-13 12:08:00 +0000

    llmv-c-search

---
+++ search.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+    "fmt"
+    "strings"
+	"github.com/gocolly/colly/v2"
+)
+
+func main() {
+
+    c := colly.NewCollector()
+
+	c.OnHTML("table.memberdecls a.el", func(e *colly.HTMLElement) {
+		link := e.Attr("href")
+
+        if !(strings.HasPrefix(link, "group__") && strings.HasSuffix(link, ".html")) {
+            return
+        }
+
+		// fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+        fmt.Printf("%s\n%s\n", e.Text, strings.Repeat("-", len(e.Text)))
+		c.Visit(e.Request.AbsoluteURL(link))
+        fmt.Println()
+	})
+
+    c.OnHTML("table.memberdecls", func(e *colly.HTMLElement) {
+        tableTitle := e.ChildText("tbody > .heading h2")
+
+        if tableTitle != "Functions" {
+            return
+        }
+
+        fmt.Println(tableTitle)
+
+        e.ForEach("tr", func(_ int, el *colly.HTMLElement) {
+            cname := el.Attr("class")
+            if strings.HasPrefix(cname, "separator") || cname == "Heading" {
+                return
+            } else if strings.HasPrefix(cname, "memitem") {
+                fmt.Printf("%s ", el.ChildText(".memItemLeft"))
+                fmt.Println(el.ChildText(".memItemRight"))
+            } else if strings.HasPrefix(cname, "memdesc") {
+                fmt.Println(el.ChildText(".mdescRight"))
+            }
+        })
+
+    })
+
+	c.OnRequest(func(r *colly.Request) {
+		// fmt.Println("Visiting", r.URL.String())
+	})
+
+	c.Visit("https://llvm.org/doxygen/group__LLVMC.html")
+
+}
